00893ba54826081cc845ad81c3c5f82c7a17d0f2
Joakim Svensson <svensson.joakim@gmail.com>
2017-11-02 23:04:38 +0100

    Add solution for 7.2

--- 2016/7/app.js
+++ 2016/7/app.js
@@ -8,9 +8,14 @@ var input = [
 var fs = require('fs');
 input = fs.readFileSync('./input.txt').toString().split('\n').filter(s => s !== '')
 
-console.log(input.filter(s => isSupportingTSL(s)).length)
+// console.log('task 1:', input.filter(s => isSupportingTSL(s)).length)
 
 function isSupportingTSL(s) {
+    var {hypernets, ipParts} = parseIp(s);
+    return hypernets.every(h => !containsAbba(h)) && ipParts.some(i => containsAbba(i))
+}
+
+function parseIp(s) {
     var hypernets = [];
     var ipParts = [];
 
@@ -24,7 +29,10 @@ function isSupportingTSL(s) {
             ipParts.push(newSplit[1])
         }
     })
-    return hypernets.every(h => !containsAbba(h)) && ipParts.some(i => containsAbba(i))
+    return {
+        hypernets,
+        ipParts
+    }
 }
 
 function containsAbba(s) {
@@ -44,3 +52,47 @@ function containsAbba(s) {
 function isAbba(s) {
     return s.charAt(0) === s.charAt(3) && s.charAt(1) === s.charAt(2) && s.charAt(0) !== s.charAt(1);
 }
+
+function isSupportingSSL(ip) {
+    var {hypernets, ipParts} = parseIp(ip)
+    var allAbas = ipParts.reduce((a, b) => {
+        return a.concat(getAllABAs(b))
+    }, [])
+    return hypernets.some(h => {
+        return allAbas.some(aba => containsBAB(h, aba))
+    })
+}
+
+function getAllABAs(s) {
+    if (s.length < 3) {
+        return []
+    }
+
+    result = []
+    var i = 0
+    while (i+2 < s.length) {
+        var substring = s.substr(i, 3)
+        if (isAba(substring)) {
+            result.push(substring)
+        }
+        i++
+    }
+    return result
+}
+
+function isAba(s) {
+    return s.charAt(0) === s.charAt(2) && s.charAt(0) !== s.charAt(1)
+}
+
+function containsBAB(s, aba) {
+    var bab = aba.charAt(1) + aba.charAt(0) + aba.charAt(1)
+    return s.includes(bab)
+}
+
+var task2Input = [
+    'aba[bab]xyz',
+    'xyx[xyx]xyx',
+    'aaa[kek]eke',
+    'zazbz[bzb]cdb',
+]
+console.log(input.filter(i => isSupportingSSL(i)).length)
